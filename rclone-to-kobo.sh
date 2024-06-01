fb531cce39a98436909e56c518eb8874ac8cd69a
GiGi <gigi@hurtault.dev>
2024-06-01 17:12:23 -0400

    add comment for possible future change

--- rclone-to-kobo.sh
+++ rclone-to-kobo.sh
@@ -7,3 +7,5 @@ ls /home/gigi/Sync/Kobo | grep -v "kepub" | xargs rm
 
 # Copy to Dropbox
 rclone copy /home/gigi/Sync/Kobo "Dropbox:Apps/Rakuten Kobo/rclone" --verbose >> /home/gigi/Sync/.rclone_logs/output.log
+
+# TODO? Add deletion to clean folder? Not sure yet.
