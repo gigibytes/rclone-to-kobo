07b65007024bef48a3c28a49ba8cf831cd0be38a
GiGi <gigi@hurtault.dev>
2024-06-01 16:59:08 -0400

    add kepub conversion and cleanup prior to upload

--- rclone-to-kobo.sh
+++ rclone-to-kobo.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+#Convert to .kepub for performance
+kpubify -iu /home/gigi/Sync/Kobo/
 
+# Clean up copied original epubs
+ls /home/gigi/Sync/Kobo | grep -v "kepub" | xargs rm
+
+# Copy to Dropbox
 rclone copy /home/gigi/Sync/Kobo "Dropbox:Apps/Rakuten Kobo/rclone" --verbose >> /home/gigi/Sync/.rclone_logs/output.log
