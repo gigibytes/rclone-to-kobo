64e8dc88a7b6885931d65ba798f6c47daa921a31
GiGi <gigi@hurtault.dev>
2024-06-02 14:20:35 -0400

    fix rm bug (spaces broke command), set dir

--- rclone-to-kobo.sh
+++ rclone-to-kobo.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+cd ""/home/gigi/Sync/Kobo
+
 if [[ -z "$(ls /home/gigi/Sync/Kobo)" ]]; then
     echo "~/Sync/Kobo directory empty, doing nothing and exiting."
     exit 0
@@ -11,7 +13,7 @@ fi
 kpubify -iu /home/gigi/Sync/Kobo/
 
 # Clean up copied original epubs
-ls /home/gigi/Sync/Kobo | grep -v "kepub" | xargs rm
+rm "$(ls /home/gigi/Sync/Kobo | grep -v "kepub")"
 
 # Copy to Dropbox
 rclone copy /home/gigi/Sync/Kobo "Dropbox:Apps/Rakuten Kobo/rclone" --verbose >> /home/gigi/Sync/.rclone_logs/output.log
