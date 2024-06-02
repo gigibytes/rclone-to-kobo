98fffcb8530eedfc3c329d861ab7d4d36ffaefe1
GiGi <gigi@hurtault.dev>
2024-06-01 23:12:45 -0400

    clean up comments

--- rclone-to-kobo.sh
+++ rclone-to-kobo.sh
@@ -2,13 +2,11 @@
 
 set -euo pipefail
 
-#TODO add test for files. if there are no files, don't do anything.
 if [[ -z "$(ls /home/gigi/Sync/Kobo)" ]]; then
     echo "~/Sync/Kobo directory empty, doing nothing and exiting."
     exit 0
 fi
 
-# Execution will continue if the above test fails.
 # Convert to .kepub for performance
 kpubify -iu /home/gigi/Sync/Kobo/
 
@@ -17,5 +15,3 @@ ls /home/gigi/Sync/Kobo | grep -v "kepub" | xargs rm
 
 # Copy to Dropbox
 rclone copy /home/gigi/Sync/Kobo "Dropbox:Apps/Rakuten Kobo/rclone" --verbose >> /home/gigi/Sync/.rclone_logs/output.log
-
-# TODO? Add deletion to clean folder? Not sure yet.
